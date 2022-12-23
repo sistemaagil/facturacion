fac023d7e26c91384ec9326a787986d25a2d9a91
freddyheredia4 <freddyheredia4@gmail.com>
2022-12-23 17:30:45 -0500

    agrega authz

---
+++ authz/src/main/java/facturacion/authz/security/UserController.java
@@ -0,0 +1,23 @@
+package facturacion.authz.security;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/user")
+@CrossOrigin({"*"})
+public class UserController {
+    
+    @Autowired UserService userService;
+
+    @GetMapping("/")
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+}
